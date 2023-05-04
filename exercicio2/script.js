7e030b54e564e6e7da3312dd116b252d048a05f0
Daylton Araujo <dayltongg46@gmail.com>
2023-05-03 21:21:36 -0300

    exercicio1

---
+++ exercicio2/script.js
@@ -0,0 +1,6 @@
+let pergunta1 = prompt("Você toma café?")
+let pergunta2 = prompt("Você é legal?")
+let pergunta3 = prompt("tudo bem?")
+
+console.log("Você toma café?", pergunta1, "Você é legal?", pergunta2, "tudo bem?3", pergunta3)
+
